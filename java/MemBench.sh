2f369795644343808ab3e7319c2a6861c943a9cc
Fabio Di Peri <fabio@kipters.dev>
2023-05-30 20:40:27 +0200

    :bug: Correctly pass cmdline args to JVM

--- java/MemBench.sh
+++ java/MemBench.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
-$SCRIPT_DIR/../jdk/bin/java -Xms1g -Xmx8g -jar $SCRIPT_DIR/app.jar
+$SCRIPT_DIR/../jdk/bin/java -Xms1g -Xmx8g -jar $SCRIPT_DIR/app.jar $@
